0eea2e997984957eeba820274dbfa69456dce40b
zszazi <41579863+zszazi@users.noreply.github.com>
2019-09-24 11:33:08 +0530

    Update client.js

--- app/static/client.js
+++ app/static/client.js
@@ -29,7 +29,6 @@ function analyze() {
   xhr.onload = function(e) {
     if (this.readyState === 4) {
       var response = JSON.parse(e.target.responseText);
-      
       el("result-label").innerHTML = `Result = ${response["result"]}`;
     }
     el("analyze-button").innerHTML = "Analyze";
@@ -39,4 +38,3 @@ function analyze() {
   fileData.append("file", uploadFiles[0]);
   xhr.send(fileData);
 }
-
